174cd54a454f0f533469e45136a1fc370c7a9396
Minh <35814521+pierre9009@users.noreply.github.com>
2022-06-13 23:16:24 +0200

    Add files via upload

---
+++ maths.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import time
+output='.\maths\doc'
+site="https://ronan.lauvergnat.fr/Enseignements_actuels_RL.html"
+ht=requests.get(site)
+soup=BeautifulSoup(ht.text, 'html.parser')
+
+for a in soup.find_all('a', class_="doc"):
+	time.sleep(0.5)
+	print("Found the URL:", "https://ronan.lauvergnat.fr/"+a['href'])
+	temp="https://ronan.lauvergnat.fr/"+a['href']
+	tempo=requests.get(temp)
+	file_path= os.path.join(output, os.path.basename(temp))
+	with open(file_path,'wb') as f:
+		f.write(tempo.content)
